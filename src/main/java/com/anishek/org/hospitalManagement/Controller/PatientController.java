9fd3583cdf0b575e6f1cadcca890ec26f4e522ff
Anishek <anisheksingh1540@gmail.com>
2026-05-16 14:56:10 +0530

    Added Patient Controller

---
+++ src/main/java/com/anishek/org/hospitalManagement/Controller/PatientController.java
@@ -0,0 +1,13 @@
+package com.anishek.org.hospitalManagement.Controller;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("/patients")
+
+public class PatientController {
+
+}
